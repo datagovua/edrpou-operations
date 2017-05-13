363e33f5dac09fd0d904240feec8e0a5de3067ce
vanuan <vanuan@gmail.com>
2017-05-13 18:34:40 +0300

    Add piwik

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,7 @@
-scp deploy.sh root@46.101.102.160:deploy.yml
-ssh root@46.101.102.160 docker stack deploy --compose-file deploy.yml edr
+FILE=${1:-deploy.yml}
+STACK=${2:-edr}
+echo $FILE
+echo $STACK
+
+scp $FILE root@46.101.102.160:$FILE
+ssh root@46.101.102.160 docker stack deploy --compose-file $FILE $STACK
