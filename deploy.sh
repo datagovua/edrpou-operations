995065ae4412b507f9a386ef1e8e154b690aa4c3
vanuan <vanuan@gmail.com>
2018-11-15 05:31:28 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
+eval $(docker-machine env do-edr3)
+
 FILE=${1:-deploy.yml}
 STACK=${2:-edr}
 echo $FILE
 echo $STACK
 
-scp $FILE root@46.101.102.160:$FILE
-ssh root@46.101.102.160 docker stack deploy --compose-file $FILE $STACK
+docker stack deploy --compose-file $FILE $STACK
